eccc55412e5e167f71d1bc1c1838700d19116611
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-04-20 00:26:37 +0800

    chore: base setup

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+    <section class="h-screen w-screen flex justify-center items-center">
+        arca!
+    </section>
+`;
