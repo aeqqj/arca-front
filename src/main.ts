13e26fac42e7bb30775f02d3d57a02d86aed7206
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-04-20 22:31:10 +0800

    feat: added static landing page

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <section class="h-screen w-screen flex justify-center items-center">
-        arca!
-    </section>
+    hallo
 `;
