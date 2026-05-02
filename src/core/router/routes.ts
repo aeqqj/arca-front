8fef777c2480a880bd4bfa0202d84c8621707605
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-05-02 20:04:07 +0800

    feat: added auth pages

--- src/core/router/routes.ts
+++ src/core/router/routes.ts
@@ -1,12 +1,17 @@
 import { LandingPage } from "../../features/landing/landing-page.ts";
+import { LoginPage } from "../../features/auth/login-page.ts";
+import { RegisterPage } from "../../features/auth/register-page.ts";
 
 export const routes: Record<string, { page: () => void } > = {
     '/': {
         page: LandingPage,
     },
-    // 'auth/signup': {
-    //     page: 
-    // }
+    'auth/signin': {
+        page: LoginPage,
+    },
+    'auth/signup': {
+        page: RegisterPage,
+    }
     // 404: {
     //     page: ErrorPage,
     // }
