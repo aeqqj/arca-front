4575d48c6b1b1fafae88a2810fc95cd4c81e8651
aeqqj <48713075+aeqqj@users.noreply.github.com>
2026-09-03 19:25:16 +0800

    feat: full create ui

--- src/features/create-post/components/createLinks.ts
+++ src/features/create-post/components/createLinks.ts
@@ -1,12 +1,51 @@
+// export function createLinks() {
+//     return `
+//         <div class="w-100 flex flex-col gap-2">
+//             <label for="links" class="text-fg2">Links</label>
+//             <div class="bg-bg2 border border-dashed border-border hover:bg-bg3/60 focus:border-fg5/60 rounded-xs h-60 flex items-center justify-center relative transition-colors p-6">
+//                 <input type="url" id="links" class="absolute inset-0 opacity-0 cursor-pointer" placeholder="">
+//                 <button type="button" class="w-10 h-10 flex items-center justify-center rounded-xs bg-bg-icon pointer-events-none">
+//                     <i data-lucide="link" class="w-4 h-4 text-fg-icon"></i>
+//                 </button>
+//             </div>
+//         </div>
+//     `
+// }
+
 export function createLinks() {
     return `
         <div class="w-100 flex flex-col gap-2">
-            <label for="attachments" class="text-fg2">Attachments</label>
-            <div class="bg-bg2 border border-dashed border-border hover:bg-bg3/60 focus:border-fg5/60 rounded-xs h-60 flex items-center justify-center relative transition-colors">
-                <input type="file" id="attachments" class="absolute inset-0 opacity-0 cursor-pointer" multiple>
-                <button type="button" class="w-10 h-10 flex items-center justify-center rounded-xs bg-bg-icon pointer-events-none">
-                    <i data-lucide="paperclip" class="w-4 h-4 text-fg-icon"></i>
+            <div class="w-full flex justify-between items-center">
+                <label for="links" class="text-fg2">Links</label>
+                <button type="button" class="p-2 hover:bg-bg5/60 transition-colors">
+                    <i data-lucide="plus" class="w-4.5 h-4.5 text-fg2 pointer-events-none"></i>
                 </button>
+            </div>
+            <div class="h-fit bg-bg2 border border-dashed border-border hover:bg-bg3/60 rounded-xs flex flex-col items-center justify-center relative transition-colors p-6 gap-4">
+                <div class="w-full h-fit border border-border bg-bg4 p-4 flex gap-2 items-center hover:bg-bg5/80 transition-colors relative">
+                    <button type="button" class="group absolute -top-2 -right-2 w-5 h-5 flex items-center justify-center rounded-xs bg-bg5 border border-border hover:bg-bg4 transition-colors">
+                        <i data-lucide="x" class="w-4 h-4 text-fg5 group-hover:text-fg3 pointer-events-none transition-colors"></i>
+                    </button>
+                    <div class="w-fit h-fit p-2.5 bg-bg-icon">
+                        <i data-lucide="play" class="w-4 h-4 text-fg-icon"></i>
+                    </div>
+                    <div class="flex flex-col min-w-0 flex-1">
+                        <p class="text-sm text-fg3 truncate">Spacey Electro & Techno Mix in the Twilight Zone | Tinzo</p>
+                        <p class="text-xs text-fg5 truncate">youtube.com</p>
+                    </div>
+                </div>
+                <div class="w-full h-fit border border-border bg-bg4 p-4 flex gap-2 items-center hover:bg-bg5/80 transition-colors relative">
+                    <button type="button" class="group absolute -top-2 -right-2 w-5 h-5 flex items-center justify-center rounded-xs bg-bg5 border border-border hover:bg-bg4 transition-colors">
+                        <i data-lucide="x" class="w-4 h-4 text-fg5 group-hover:text-fg3 pointer-events-none transition-colors"></i>
+                    </button>
+                    <div class="w-fit h-fit p-2.5 bg-bg-icon">
+                        <i data-lucide="play" class="w-4 h-4 text-fg-icon"></i>
+                    </div>
+                    <div class="flex flex-col min-w-0 flex-1">
+                        <p class="text-sm text-fg3 truncate">Spacey Electro & Techno Mix in the Twilight Zone | Tinzo</p>
+                        <p class="text-xs text-fg5 truncate">youtube.com</p>
+                    </div>
+                </div>
             </div>
         </div>
     `
--- src/features/create-post/create-post-page.ts
+++ src/features/create-post/create-post-page.ts
@@ -3,8 +3,8 @@ import { header } from "../../shared/components/header.ts";
 import { sideBar } from "../../shared/components/sideBar.ts";
 import { bindYoutubeLinks } from "../../shared/handlers/youtubeHandler.ts";
 import { textEditor, initTextEditor } from "./components/textEditor.ts";
-import { createAttachments } from "./components/createAttachments.ts";
 import { createLinks } from "./components/createLinks.ts";
+import { createAttachments } from "./components/createAttachments.ts";
 
 export function CreatePostPage() {
 	const app = document.querySelector<HTMLDivElement>("#app")!;
@@ -18,7 +18,7 @@ export function CreatePostPage() {
                         <h1 class="text-fg1 font-medium">Create Post</h1>
                         <button class="w-fit h-fit px-3 py-2 text-fg3 text-body-lg border border-border hover:bg-bg3 transition-colors">Pick a course</button>
                     </div>
-                    <form action="" class="w-200 flex flex-col gap-8">
+                    <form action="" class="w-200 flex flex-col gap-8 text-fg3">
                         <div class="flex flex-col gap-2">
                             <label for="" class="text-fg2">Title</label>
                             <input type="" class="p-4 h-fit w-full border border-border bg-bg2 hover:bg-bg3/60 focus:border-fg5/60 transition-colors">
@@ -32,6 +32,12 @@ export function CreatePostPage() {
                                 </div>
                             </div>
                         </div>
+                        <div class="text-fg2 flex flex-col gap-2">
+                            <p>Image</p>
+                            <div class="border border-border relative w-full aspect-video bg-bg4 hover:bg-bg5/80 transition-colors"> 
+                                <img src="https://img.youtube.com/vi/4WfSohJ9K5o/maxresdefault.jpg" class="w-full h-full object-cover" />
+                            </div>
+                        </div>
                         ${textEditor()}
                         <div class="flex gap-2 w-full">
                             <button class="w-full bg-bg3 text-fg2 border border-border py-3 rounded-xs hover:opacity-90 transition-all">Save Draft</button>
@@ -40,8 +46,8 @@ export function CreatePostPage() {
                     </form>
                 </div>
                 <div class="flex flex-col gap-8">
-                    ${createLinks()}
                     ${createAttachments()}
+                    ${createLinks()}
                 </div>
             </div>
         </div>
